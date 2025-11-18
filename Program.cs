feaf8a2b2a1af4e8240cd746c89d7ccd235201ca
Nicola Iarocci <nicola@nicolaiarocci.com>
2025-11-18 16:22:46 +0100

    cleanup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.FeatureManagement;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services
     .AddFeatureManagement(builder.Configuration.GetSection("FeatureFlags"));
 
