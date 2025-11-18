eac131097a2fc6672cc52dfba21987e05a37b154
Nicola Iarocci <nicola@nicolaiarocci.com>
2025-11-18 16:09:47 +0100

    cleanup

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.FeatureManagement;
 using Microsoft.FeatureManagement.FeatureFilters;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddHttpContextAccessor();
 builder.Services
     .AddFeatureManagement(builder.Configuration.GetSection("FeatureFlags"))
