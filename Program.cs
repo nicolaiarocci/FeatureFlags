b12df4f7913d6f508d5c099cedab4023d9a598a9
Nicola Iarocci <nicola@nicolaiarocci.com>
2025-11-19 16:12:22 +0100

    Remove emojis from README

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services
 var app = builder.Build();
 
 var weatherforecastGroup = app.MapGroup("/weatherforecast")
-    .AddEndpointFilter(new FeatureFilter("WeatherForecast"));
+    .AddEndpointFilter(new EndpointFilter("WeatherForecast"));
 
 var summaries = new[]
 {
